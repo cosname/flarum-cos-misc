76917f61b08742d81e876f91ad1a0ec54fa96203
Yixuan Qiu <yixuan.qiu@cos.name>
2019-11-27 17:23:03 -0500

    prevent spams

--- src/Listener/CheckEmailDomain.php
+++ src/Listener/CheckEmailDomain.php
@@ -8,6 +8,35 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 class CheckEmailDomain
 {
+    // Black list of email domains
+    private static $blacklist = [
+        'yxpf.xyz',
+        '263mali.cn',
+        '120mail.com',
+        'huaweimali.cn',
+        'oiizz.com',
+        'dawin.com',
+        'eveav.com',
+        'juyouxi.com',
+        'jnpayy.com',
+        'moakt.ws',
+        '119mail.com',
+        'yopmail.com',
+        'alivemail.ga',
+        'master-mail.net'
+    ];
+
+    private function blacklist_regex()
+    {
+        // Escape dot
+        $regex = str_replace('.', '\\.', self::$blacklist);
+        // Suffix
+        $regex = preg_replace('/$/', '$', $regex);
+        // Merge rules
+        $regex = implode('|', $regex);
+        return $regex;
+    }
+
     /**
      * @param Dispatcher $events
      */
@@ -23,11 +52,17 @@ class CheckEmailDomain
     {
         // https://discuss.flarum.org/d/4238-how-to-add-custom-validate-for-post
         if ($event->type instanceof UserValidator) {
+            // The 'regex' rule is used to detect QQ mails, associated with a warning message
+            // The 'not_regex' rule is used to prevent spams
             $event->validator->addRules([
-                'email' => 'regex:/.*(?<!qq\\.com)$/i'
+                'email' => [
+                    'regex:/.*(?<!qq\\.com)$/i',  // see https://stackoverflow.com/a/16398813
+                    'not_regex:/' . $this->blacklist_regex() . '/i'
+                ]
             ]);
             $event->validator->setCustomMessages([
                 'email.regex' => '请避免使用 QQ 邮箱',
+                'email.not_regex' => '请证明你不是机器人！'
             ]);
         }
     }
